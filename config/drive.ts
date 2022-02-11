6ba4c994ef63f51a52cb439676e57ed5fe8c4778
Gabriel Lisboa <gabrielbento.rx@outlook.com>
2022-02-11 18:07:10 -0300

    feat: Adding s3 driver configuration

--- config/drive.ts
+++ config/drive.ts
@@ -18,6 +18,7 @@ import Application from '@ioc:Adonis/Core/Application'
 | defined inside the `contracts` directory.
 |
 */
+
 const driveConfig: DriveConfig = {
   /*
   |--------------------------------------------------------------------------
@@ -31,77 +32,23 @@ const driveConfig: DriveConfig = {
   disk: Env.get('DRIVE_DISK'),
 
   disks: {
-    /*
-    |--------------------------------------------------------------------------
-    | Local
-    |--------------------------------------------------------------------------
-    |
-    | Uses the local file system to manage files. Make sure to turn off serving
-    | files when not using this disk.
-    |
-    */
     local: {
       driver: 'local',
       visibility: 'public',
-
-      /*
-      |--------------------------------------------------------------------------
-      | Storage root - Local driver only
-      |--------------------------------------------------------------------------
-      |
-      | Define an absolute path to the storage directory from where to read the
-      | files.
-      |
-      */
       root: Application.tmpPath('uploads'),
-
-      /*
-      |--------------------------------------------------------------------------
-      | Serve files - Local driver only
-      |--------------------------------------------------------------------------
-      |
-      | When this is set to true, AdonisJS will configure a files server to serve
-      | files from the disk root. This is done to mimic the behavior of cloud
-      | storage services that has inbuilt capabilities to serve files.
-      |
-      */
-      serveFiles: true,
-
-      /*
-      |--------------------------------------------------------------------------
-      | Base path - Local driver only
-      |--------------------------------------------------------------------------
-      |
-      | Base path is always required when "serveFiles = true". Also make sure
-      | the `basePath` is unique across all the disks using "local" driver and
-      | you are not registering routes with this prefix.
-      |
-      */
       basePath: '/uploads',
+      serveFiles: true,
     },
 
-    /*
-    |--------------------------------------------------------------------------
-    | S3 Driver
-    |--------------------------------------------------------------------------
-    |
-    | Uses the S3 cloud storage to manage files. Make sure to install the s3
-    | drive separately when using it.
-    |
-    |**************************************************************************
-    | npm i @adonisjs/drive-s3
-    |**************************************************************************
-    |
-    */
-    // s3: {
-    //   driver: 's3',
-    //   visibility: 'public',
-    //   key: Env.get('S3_KEY'),
-    //   secret: Env.get('S3_SECRET'),
-    //   region: Env.get('S3_REGION'),
-    //   bucket: Env.get('S3_BUCKET'),
-    //   endpoint: Env.get('S3_ENDPOINT'),
-    // },
+    s3: {
+      driver: 's3',
+      visibility: 'public',
+      key: Env.get('S3_KEY'),
+      secret: Env.get('S3_SECRET'),
+      region: Env.get('S3_REGION'),
+      bucket: Env.get('S3_BUCKET'),
+      endpoint: Env.get('S3_ENDPOINT'),
+    },
 
     /*
     |--------------------------------------------------------------------------
