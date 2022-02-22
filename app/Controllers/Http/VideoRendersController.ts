a97033b7d110eb62277dcec45fed5370b99a375e
Gabriel Lisboa <gabrielbento.rx@outlook.com>
2022-02-21 21:06:19 -0300

    reject

--- app/Controllers/Http/VideoRendersController.ts
+++ app/Controllers/Http/VideoRendersController.ts
@@ -1,3 +1,16 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import TemplateCustomization from 'App/Models/TemplateCustomization'
 
-export default class VideoRendersController {}
+export default class VideoRendersController {
+  public async index({ request }: HttpContextContract) {
+    const key = request.param('key')
+
+    const templateToBeRendered = await TemplateCustomization.findByOrFail('key', key)
+
+    const payloadProcessed = this.preparePayloadForRendering(key, templateToBeRendered.payload)
+  }
+
+  private preparePayloadForRendering(key: string, payload: object) {
+    console.log({ key, payload })
+  }
+}
